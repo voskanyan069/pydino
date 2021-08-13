9f8480935fc6539637f4deaabc08e9853afcd80a
Andranik Voskanyan <andranik.voskanyan@realschool.am>
2021-08-13 18:48:04 +0400

    added new feature: play with mouse added new button in menu 	Mouse control: True/False

--- main.py
+++ main.py
@@ -23,11 +23,14 @@ pygame.display.set_icon(app_icon) # set app icon
 
 menu = True # is menu showing
 game = True # is game running
+mouse_play = True # is game runned in mouse mode
 scores = 0 # game scores
 high_score = 0 # all time high score
 play_time = 0 # game play time seconds
 health = 5 # game healths
 data = 0 # temp start value for data
+font_family = f'{assets}/font.otf' # font path
+mouse_control_btn = 0 # define the button for global use
 
 class Dino:
     def __init__(self, x, y):
@@ -46,6 +49,10 @@ class Dino:
             self.x -= self.speed
         elif direction == 1 and self.x <= width-50: # moved right
             self.x += self.speed
+        print(f'Dino moved to [{self.x};{self.y}]') # info log
+
+    def move_to_position(self, x):
+        self.x = x
         print(f'Dino moved to [{self.x};{self.y}]') # info log
 
     def add_speed(self, speed):
@@ -102,6 +109,7 @@ class Part:
     def move(self):
         global scores
         global health
+
         self.dx, self.dy, self.dw, self.dh = \
                 self.dino.get_params() # get geometry params of dino
         self.y += self.speed # move down the part
@@ -178,11 +186,12 @@ class DamageFood(Part):
 
 class Text:
     def __init__(self, text, font_size, position):
+        global font_family
+        
         self.text = text # text to display
-        self.font_family = f'{assets}/font.otf' # font path
         self.font_size = font_size # font size
         self.text_color = (56,61,61) # text color
-        self.font = pygame.font.Font(self.font_family, self.font_size) # font
+        self.font = pygame.font.Font(font_family, self.font_size) # font
         self.img = self.font.render(text, True, self.text_color) # rendered text
         self.position = position # text position
         print(f'Created text - {text}') # info log
@@ -195,25 +204,46 @@ class Text:
         self.img = self.font.render(text, True, self.text_color) # rendered text
 
 class MyButton:
-    def __init__(self, btn_y, text, on_click):
-        btn_w = 150 # button width
-        btn_h = 50  # button height
-        btn_x = (width // 2) - (btn_w // 2) # center button by x
-        btn_radius = 10 # button radius
-        color_1 = (33,158,188) # button default color
-        color_2 = (4,107,159)  # button hover color
-        color_3 = (3,81,119)   # button click color
+    def __init__(self, params, text, on_click, font_size=30,
+            radius=10, is_center=True):
+        global font_family
+        
+        self.btn_x = params[0] # button x position
+        self.btn_y = params[1] # button y position
+        self.btn_w = params[2] # button width
+        self.btn_h = params[3] # button height
+        self.radius = radius   # button corner radius
+        self.font = pygame.font.Font(font_family, font_size) # font family
+        self.text = text # button text
+        self.on_click = on_click # on click method
+        if is_center: # if button placed on center
+            self.btn_x = (width // 2) - (self.btn_w // 2) # center button by x
+        self.color_1 = (33,158,188) # button default color
+        self.color_2 = (4,107,159)  # button hover color
+        self.color_3 = (3,81,119)   # button click color
 
         self.button = Button( # create button
-            screen, btn_x, btn_y, btn_w, btn_h, text=text, fontSize=40,
-            inactiveColour=color_1, hoverColour=color_2,
-            pressedColour=color_3, radius=btn_radius,
-            onClick=on_click
+            screen, self.btn_x, self.btn_y, self.btn_w, self.btn_h,
+            text=self.text, font=self.font,
+            inactiveColour=self.color_1, hoverColour=self.color_2,
+            pressedColour=self.color_3, radius=self.radius,
+            onClick=self.on_click
         )
+        print(f'{text} button cerated with {params} parameters') # info log
+
+    def set_text(self, new_text):
+        self.button = Button( # override button
+            screen, self.btn_x, self.btn_y, self.btn_w, self.btn_h,
+            text=new_text, font=self.font,
+            inactiveColour=self.color_1, hoverColour=self.color_2,
+            pressedColour=self.color_3, radius=self.radius,
+            onClick=self.on_click
+        )
+        print(f'{self.text} button text changed {new_text}') # info log
 
 class File:
     def __init__(self, filename):
-        self.filename = filename
+        self.filename = filename # set filename
 
     def read_data(self, key):
         with open(self.filename, mode='r') as file: # read file content
@@ -237,9 +267,9 @@ def main(argv):
     try:
         opts, args = getopt.getopt(argv,"hc",["help","clear"]) # get args
     except getopt.GetoptError: # if error on args getting
-        print('\n\nUsage: dino.py -h/--help | to show help message')
+        print('\n\nUsage: dino.py -h/--help | to show help message') # help
         sys.exit(2) # exit app with error code 2
-    for opt, arg in opts:
+    for opt, arg in opts: # loop in arguments
         if opt == '-h' or opt == '--help': # -h/--help
             print('\n\n') # two lines
             print('Dino'.center(80)) # print app name in terminal center
@@ -261,11 +291,16 @@ def main(argv):
 
 def show_menu():
     global menu
+    global mouse_control_btn
     menu = True
 
-    start_btn = MyButton(120, 'Play', on_start_click).button # play button init
-    exit_btn = MyButton(180, 'Exit', on_exit_click).button # exit button init
-    high_score_text = Text(f'High-Score: {high_score}', 30, ((width//2)-70,240))
+    # menu buttons initialization
+    start_btn = MyButton((0,120,150,50), 'Play', on_start_click).button
+    exit_btn = MyButton((0,180,150,50), 'Exit', on_exit_click).button
+    high_score_text = Text(f'High-Score: {high_score}', 24, ((width//2)-65,240))
+    mouse_control_btn = MyButton((width-150,10,140,30),
+        f'Mouse control: {mouse_play}', on_mouse_control_click,
+        font_size=18, radius=5, is_center=False)
 
     while menu:
         events = pygame.event.get()
@@ -279,26 +314,39 @@ def show_menu():
             menu = False
 
         screen.fill(bg_color) # update background color
-        high_score_text.update()
+        high_score_text.update() # update high score text
         pw.update(events) # update pygame_widgetes
 
         update_frame() # update frame
 
 def on_start_click():
     global menu
+
+    print('Game started') # info log
     menu = False # stop menu
     play() # start game
 
 def on_exit_click():
+    print('Game closed') # info log
     # close the window
-    pygame.quit()
-    quit()
+    pygame.quit() # close pygame
+    quit() # close python
+
+def on_mouse_control_click():
+    global mouse_play
+    global mouse_control_btn
+
+    mouse_play = not mouse_play # reverse mouse control optrion
+    mouse_control_btn.set_text(f'Mouse control: {mouse_play}') # change text
+    print(f'Mouse control switched to {mouse_play}') # info log
 
 def play():
     # game
     global game
     global health
     global play_time
+    global mouse_play
+
     # reset game options
     game = True
     scores = 0
@@ -326,11 +374,11 @@ def play():
     health_text = Text('Health: 5', 22, (10, 70)) # health text
 
     # parts
-    parts = []
-    for i in range(random.randint(2, 4)):
-        parts.append(DinoPart(dino, scores_text, health_text))
-    for i in range(random.randint(1, 2)):
-        parts.append(DamageFood(dino, scores_text, health_text))
+    parts = [] # list of parts
+    for i in range(random.randint(2, 4)): # create 3-5 parts
+        parts.append(DinoPart(dino, scores_text, health_text)) # add new part
+    for i in range(random.randint(1, 2)): # create 2-3 mines
+        parts.append(DamageFood(dino, scores_text, health_text)) # add new mine
 
     # launch new threads
     _thread.start_new_thread(update_time, ()) # play time thread
@@ -347,10 +395,14 @@ def play():
         if keys[pygame.K_ESCAPE]:
             game = False
         # movement
-        if keys[pygame.K_LEFT]: # <-
-            dino.move(0)
-        elif keys[pygame.K_RIGHT]: # ->
-            dino.move(1)
+        if mouse_play:
+            mx, _ = pygame.mouse.get_pos() # get current mouse position
+            dino.move_to_position(mx) # move dino to current mouse x in window
+        else:
+            if keys[pygame.K_LEFT]: # <-
+                dino.move(0)
+            elif keys[pygame.K_RIGHT]: # ->
+                dino.move(1)
         # update frame
         screen.fill(bg_color) # update background color
         dino.show() # move dino
@@ -359,6 +411,7 @@ def play():
         for part in parts: # loop all parts
             part.move() # start part movement
         
+        # refresh texts
         scores_text.update() # refresh scores
         time_text.update() # refresh time
         health_text.update() # refresh time
@@ -371,12 +424,12 @@ def end_game():
     global high_score
     global scores
     global data
-
     game = False # stop game window
+
     if high_score < scores: # if last game scores greather than high score
         data.write_data('high_score', scores) # rewrite high score
         high_score = scores # update high score
-    scores = 0
+    scores = 0 # reset scores
 
 def update_frame():
     pygame.display.update() # update frame
@@ -392,7 +445,7 @@ def update_time():
 
 def update_dino_speed(dino):
     global game
-    count = 0
+    count = 0 # how many times speed added
 
     while game and count <= 10: # repeat 10 times
         pygame.time.wait(10000) # sleep for 10 sec
